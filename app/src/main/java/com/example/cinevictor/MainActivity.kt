28c78677695415dc90632a3794394f658d28ca7d
Alumno07 <proyecto.iru.micro@gmail.com>
2024-10-04 17:16:05 +0100

    add

--- app/src/main/java/com/example/cinevictor/MainActivity.kt
+++ app/src/main/java/com/example/cinevictor/MainActivity.kt
@@ -18,6 +18,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CineVictorTheme {
+
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Column(Modifier.padding(innerPadding)) {
                         ReviewsScreen(Modifier.fillMaxSize())
