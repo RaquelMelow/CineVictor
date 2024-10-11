4e6fad011dc3ca124583db61e73ab433e5932f4f
Alumno07 <proyecto.iru.micro@gmail.com>
2024-10-11 18:31:14 +0100

    add añadir pantalla de films

--- app/src/main/java/com/example/cinevictor/MainActivity.kt
+++ app/src/main/java/com/example/cinevictor/MainActivity.kt
@@ -4,11 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
 import com.example.cinevictor.ui.theme.CineVictorTheme
 
@@ -18,13 +16,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CineVictorTheme {
-
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Column(Modifier.padding(innerPadding)) {
-                        Text("Cambio 1")
-                    }
+                    Films(Modifier.padding(innerPadding))
                 }
             }
         }
     }
 }
+
