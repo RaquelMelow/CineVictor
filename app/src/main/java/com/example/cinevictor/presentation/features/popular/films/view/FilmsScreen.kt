728a3c51c5fe4bb08d013275fc841657827a4dd9
timacosta <timacosta06@gmail.com>
2024-11-04 16:55:51 +0000

    add theme

--- app/src/main/java/com/example/cinevictor/presentation/features/popular/films/view/FilmsScreen.kt
+++ app/src/main/java/com/example/cinevictor/presentation/features/popular/films/view/FilmsScreen.kt
@@ -1,4 +1,3 @@
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -7,9 +6,9 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -19,14 +18,14 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.cinevictor.presentation.features.films.model.MovieData
+import coil.compose.AsyncImage
+import com.example.cinevictor.domain.model.Movie
 import com.example.cinevictor.presentation.features.films.viewmodel.FilmsViewModel
 
 @Composable
