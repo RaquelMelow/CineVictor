46faddf6b4b728907da9deeb70f2e3777123e468
timacosta <timacosta06@gmail.com>
2024-11-12 20:11:54 +0000

    add uiState

--- app/src/main/java/com/example/cinevictor/presentation/features/popular/journal/view/JournalRoute.kt
+++ app/src/main/java/com/example/cinevictor/presentation/features/popular/journal/view/JournalRoute.kt
@@ -1,6 +1,7 @@
 package com.example.cinevictor.presentation.features.popular.journal.view
 
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -9,16 +10,22 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import com.example.cinevictor.domain.model.Movie
 import com.example.cinevictor.presentation.features.popular.journal.viewmodel.JournalViewModel
+import com.example.cinevictor.presentation.features.popular.journal.viewmodel.UiState
 
 @Composable
 fun JournalRoute(
     viewModel: JournalViewModel
 ) {
-    val movies by viewModel.sceneListsItems.collectAsState()
+    val uiState by viewModel.sceneListsItems.collectAsState()
+    val isLoading by viewModel.isLoading.collectAsState()
     var selectedMovie by remember { mutableStateOf<Movie?>(null) }
     val state = rememberLazyListState()
 
-    movies?.let {
+    if(isLoading) {
+        CircularProgressIndicator()
+    }
+
+    uiState?.let {
         JournalScreen(
             state = state,
             movies = it,
@@ -31,4 +38,29 @@ fun JournalRoute(
             }
         )
     }
+
+
+//    when(val uiState = uiState) {
+//        is UiState.Loading -> {
+//            CircularProgressIndicator()
+//        }
+//
+//        is UiState.Success -> {
+//            uiState.movies?.let {
+//                JournalScreen(
+//                    state = state,
+//                    movies = it,
+//                    selectedMovie = selectedMovie,
+//                    onClick = { movie ->
+//                        selectedMovie = movie
+//                    },
+//                    onDismiss = {
+//                        selectedMovie = null
+//                    }
+//                )
+//            }
+//        }
+//    }
+
+
 }
--- app/src/main/java/com/example/cinevictor/presentation/features/popular/journal/viewmodel/JournalViewModel.kt
+++ app/src/main/java/com/example/cinevictor/presentation/features/popular/journal/viewmodel/JournalViewModel.kt
@@ -9,20 +9,40 @@ import com.example.cinevictor.data.repository.MovieRepository
 import com.example.cinevictor.domain.model.Movie
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import java.lang.Thread.State
+
+sealed class UiState {
+    data object Loading : UiState()
+    data class Success(val movies: List<Movie>?) : UiState()
+}
+
+data class UiState2(
+    val isLoading: Boolean = false,
+    val movies: List<Movie>? = null
+)
 
 class JournalViewModel : ViewModel() {
 
     private val movieService = RetrofitClient.retrofit.create(MovieService::class.java)
     private val repository = MovieRepository(movieService)
 
-    private val _listsItems = MutableStateFlow<List<Movie>?>(emptyList())
+    private val _listsItems = MutableStateFlow<List<Movie>?>(null)
     val sceneListsItems: StateFlow<List<Movie>?> = _listsItems
 
+    private val _isLoading = MutableStateFlow(false)
+    val isLoading: StateFlow<Boolean> = _isLoading
+
     init {
+        _isLoading.value = true
         viewModelScope.launch {
+
             val movies: List<Movie>? = repository.getUpcomingMovies(1)
+
             _listsItems.value = movies
+
+            _isLoading.value = false
         }
     }
 }
